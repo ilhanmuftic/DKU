ac53e1394a355788172d2f98dc9cb367d58076ad
SI 005 <ilhanmuftic@gmail.com>
2024-02-12 13:06:08 +0100

    Login, server setup

--- app.js
+++ app.js
@@ -36,26 +36,33 @@ app.use(bodyParser.json());
 app.use(cookieParser())  
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'home.html'));
+  res.sendFile(path.join(__dirname, 'public', 'html', 'home.html'));
 });
 
+app.get('/student', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'student.html'));
+})
+
+app.get('/professors', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'professor.html'))
+})
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
 });
 
-app.get(' *', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', '404.html'))
 })
 
 
 app.post('/login', async (req, res) => {
   try {
-      var { username, password } = req.body;
-      username = username.toLowerCase()
+      var { email, password } = req.body;
+      email = email.toLowerCase()
 
       // Find the user by username in the database
-      const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+      const result = await client.query('SELECT * FROM users WHERE Email = $1;', [email]);
 
       if (result.rows.length === 0) {
           return res.status(401).json({ error: 'Invalid username or password' });
@@ -64,19 +71,19 @@ app.post('/login', async (req, res) => {
       const user = result.rows[0];
 
       // Compare the provided password with the hashed password
-      const passwordMatch = await bcrypt.compare(password, user.password);
+      const passwordMatch = await bcrypt.compare(password, user.Password);
 
       if (!passwordMatch) {
           return res.status(401).json({ error: 'Invalid username or password' });
       }
 
       // Generate a JWT for authentication
-      const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, {
+      const token = jwt.sign({ userId: user.Id, email: user.Email }, JWT_SECRET, {
           expiresIn: '1y',
       });
       // Return the token to the client
-      var url = "/home"
-      await checkActivation(user.id) ? url = "/home" : url = "/activate";
+      var url = ""
+      await user.Type == "Student" ? url = "/student" : url = "/professor";
       res.json({ token: token, url: url });
   } catch (error) {
       console.error(error);
@@ -84,36 +91,6 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.post('/signup', async (req, res) => {
-  try {
-      var { username, password } = req.body;
-      username = username.toLowerCase()
-      console.log(username, password)
-
-      // Check if username already exists in the database
-      const existingUser = await client.query('SELECT * FROM users WHERE username = $1', [username]);
-
-      if (existingUser.rows.length > 0) {
-          return res.status(400).json({ error: 'Username already exists' });
-      }
-
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Insert the new user into the database
-      const result = await client.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id', [
-          username,
-          hashedPassword
-      ]);
-      const token = jwt.sign({ userId: result.rows[0].id, username: username }, JWT_SECRET, {
-          expiresIn: '1y',
-      });
-      res.json({ token });
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
